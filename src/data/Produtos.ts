9bf9d2fd8cffb272113baf3efd456c5a66792818
Taina <ta.cbfogaca@gmail.com>
2025-09-09 14:39:52 -0300

    Cafeteria_useAlert

--- src/data/Produtos.ts
+++ src/data/Produtos.ts
@@ -4,18 +4,21 @@ export const cafeteria =[
     description:"Recheio de frango com catupiry!",
     price:"R$ 14,50",
     image:"croassaint.jpg",
+    like:24
 },
 {
     title:"Pão de Queijo",
     description:"Pão de queijo tradicional!",
     price:"R$ 6,00",
     image:"pao.jpg",
+    like:8
 },
 {
     title:"Rocambole",
     description:"Massa branca e recheio de doce de leite!",
     price:"R$ 10,00",
     image:"rocambole.jpg",
+    like:36
 },
 
 {
@@ -23,6 +26,7 @@ export const cafeteria =[
     description:"Sabor de doce de leite cremoso!",
     price:"R$ 12,00",
     image:"capuctino.jpg",
+    like:40
 },
 
 ]
